8a48a374d632031448dbaee4e3e93de0c8d0dbe9
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-03-19 13:31:00 -0700

    Added various comments.

--- Ch2P1Example.java
+++ Ch2P1Example.java
@@ -1,10 +1,11 @@
-//This is a simple Java program. 
-//By convention, the name of the main class should match the name of the file that holds the program. 
-//You should also make sure that the capitalization of the filename matches the class name. 
+/*This is a simple Java program. 
+-By convention, the name of the main class should match the name of the file that holds the program. 
+-You should also make sure that the capitalization of the filename matches the class name.
+-This is an example of a multiline comment in Java. */
 
 public class Ch2P1Example {
-    //Your program begins with a call to main().
+    //Your program begins with a call to main(). This is an example of a single line comment. 
     public static void main(String[] args) {
         System.out.println("This is a simple Java program.");
-    }
-}
+    } //curly brace ends main method
+} //curly brace ends Ch2P1Example class definition.
