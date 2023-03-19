3af75dab4866868b6a96f435f33157316a3143ca
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-03-19 13:17:39 -0700

    Added main method and println.

---
+++ Ch2P1Example.java
@@ -0,0 +1,10 @@
+//This is a simple Java program. 
+//By convention, the name of the main class should match the name of the file that holds the program. 
+//You should also make sure that the capitalization of the filename matches the class name. 
+
+public class Ch2P1Example {
+    //Your program begins with a call to main().
+    public static void main(String[] args) {
+        System.out.println("This is a simple Java program.");
+    }
+}
