23ed7bcbb3d192c11fde96045593f0938df85efb
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-09 16:10:52 -0700

    For loop example program

---
+++ Ch2P4TheForTest.java
@@ -0,0 +1,17 @@
+/*
+ Simplest form of the for loop:
+    for(initialization; condition; iternation) statement;
+    -the initialization portion of the loop sets a loop control variable to an initial value. 
+    -the condition is a Boolean expression that tests the loop control variable. 
+    -if the outcome of the test is true, 'statement' executes and the for loops continues to iterate. 
+    -if it is false, the loop terminates. 
+    -the iteration expression determines how the loop control variable is changed each time the loop iterates. 
+ */
+
+
+public class Ch2P4TheForTest {
+    public static void main(String args[]) {
+
+    }
+    
+}
