bdbd4a028062f1c7c2645a8815b04378b610ade0
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-09 16:47:50 -0700

    completed code

--- Ch2P5BlockTest.java
+++ Ch2P5BlockTest.java
@@ -1,6 +1,16 @@
+//The following program uses a block of code as the target of a for loop. 
+
 public class Ch2P5BlockTest {
     public static void main(String args[]) {
-        
+        int x, y;
+        y = 20;
+
+        //the target of this loop is a block
+        for(x = 0; x<10; x++) {
+            System.out.println("This is x: " + x);
+            System.out.println("This is y: " + y);
+            y = y - 2;
+        }
     }
     
 }
